3cba537fb93b05f1cba3d034938276cf61612792
iflyimpilot <iflyimpilot@gmail.com>
2019-07-23 00:04:03 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let name = "Louie";
+let age = 33;
+let birthday = "November 21";
+const detroitGC = true;
+const lifeEvents = [
+  "I met my wife in New York.",
+  "I hold a commercial pilot's license.",
+  "Our first born child was born 3.5 years ago.",
+  "I moved to Michigan from New York 6 years ago."
+];
+
+if (detroitGC === true) {
+  console.log(
+    "My name is " +
+      name +
+      " and I am a student at Grand Circus in Detroit, Michigan. I am currently " +
+      age +
+      " years old and my birthday is on " +
+      birthday
+  );
+} else (detroitGC === true) {
+    console.log()
+}
